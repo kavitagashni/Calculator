92947f624bb18b7e0c3fbfac44f6a57174b103f1
kavitagashni <107977816+kavitagashni@users.noreply.github.com>
2022-09-01 14:44:07 +0530

    add disable condition

--- calculator.js
+++ calculator.js
@@ -1,20 +1,19 @@
-
 var output = document.getElementById("screen");
-var operetorclicked = false;
+var operatorclicked = false;
 var num1 = ''; //this variable used for store number.................
-var num2 = ''; //this variable used for store number...............
+var num2 = ''; //this variable used for store number operator...............
 var operat = ""; //this variable used for store operator...............
 disableOperbtn() //use for function call to operator button desabled in first........................
-equal_desable()//use for function call to equal button disable in first........................
+equal_disable()//use for function call to equal button disable in first........................
 // this function used for get number from html...........
 function myfunction(num) {
-    if (operetorclicked == false) { //contion that, if opertor is not clicked yet then all num go to the num1................................
-        num1 = num1 + num; 
+    if (operatorclicked == false) { //contion that, if opertor is not clicked yet then all num go to the num1................................
+        num1 = num1 + num;
         console.log("num1:-" + num1);
-        enablebtn() //use for function call to operator button enable in first........................
-    
+        enableOperator() //use for function call to operator button enable in first........................
+
     } else {    //contion that, if opertor is clicked then all num go to the num2...............................
-        num2 += num;   
+        num2 += num;
         console.log("num2:-" + num2)
         equal_enable()
     }
@@ -24,7 +23,7 @@ function myfunction(num) {
 //this function used for store all opertors........................
 function operatorclick(operator) {
     disableOperbtn()
-    operetorclicked = true;
+    operatorclicked = true;
     operat = operator;
     output.value += operat;
     console.log("operator" + num1 + operator + num2)
@@ -32,27 +31,25 @@ function operatorclick(operator) {
 
 // function used for delete number or operator from screen.................................
 function del() {
-    if(num=''){
-        equal_disable()
-        disableOperbtn()   
-    }
     let display = document.getElementById("screen"); //
     let value = display.value; // here value is a variable 
     value = value.slice(0, -1);// value.slice use for delete num or operator for screen one by one...................
     display.value = value;
-    if (operetorclicked == false) {
+    if (operatorclicked == false) {
         num1 = value;
-        console.log("delete:-num 1" + num1)
-        enablebtn()
+        if(num1 == ""){
+            disableOperbtn()
+        }
+        equal_disable()
+        console.log("delete:-num1" + value)
     }else {
         if (num2 == "") {
-            operetorclicked = false;
+            operatorclicked = false;
             console.log("delete:-operator" + operat)
-            enablebtn()
-            equal_desable()
+            enableOperator()
+            equal_disable()
         } else {
             num2 = num2.slice(0, -1);
-            disableOperbtn()
             console.log("delete:-num2" + num2)
         }
     }
@@ -62,10 +59,10 @@ function reset() {
     display = document.getElementById("screen").value;
     output.value = "";
     num1 = output.value;
-    operetorclicked = false;
+    operatorclicked = false;
     if (num1 == "") {
         disableOperbtn()
-        equal_desable() 
+        equal_disable()
     }
 
 }
@@ -78,17 +75,17 @@ function totle() {
         case '+':
             answer = number1 + number2;
             console.log(answer);
-            equal_desable() 
+            equal_disable()
             break;
         case '-':
             answer = number1 - number2;
             console.log(answer)
-            equal_desable() ;
+            equal_disable();
             break;
         case '*':
             answer = number1 * number2;
             console.log(answer);
-            equal_desable() 
+            equal_disable()
             break;
         case '/':
             answer = number1 / number2;
@@ -103,36 +100,35 @@ function totle() {
             break;
     }
     output.value = answer;
-
     console.log("answer:-" + answer);
     num1 = answer; // 
-    enablebtn()
+    enableOperator()
     num2 = '';
-    operetorclicked = false
+    operatorclicked = false
 }
 
 // this function used for operators disable ...................................
-function disableOperbtn() {   
+function disableOperbtn() {
     var btn = document.getElementsByClassName("operator");
-    console.log(btn);
-        for (var i = 0; i < btn.length; i++) {
-            btn[i].disabled = true;
-            console.log(btn[i])
-        }
+    // console.log(btn);
+    for (var i = 0; i < btn.length; i++) {
+        btn[i].disabled = true;
+        // console.log(btn[i])
     }
+}
 
 // this function used for operators button enable ...................................
-function enablebtn() {    
-var btn = document.getElementsByClassName("operator");
-console.log(btn);
+function enableOperator() {
+    var btn = document.getElementsByClassName("operator");
+    // console.log(btn);
     for (var i = 0; i < btn.length; i++) {
         btn[i].disabled = false;
-        console.log(btn[i])
+        // console.log(btn[i])
     }
 }
 
 //this function used for equal button enable .......................................
-function equal_desable() {
+function equal_disable() {
     var answer = document.getElementById("equal");
     answer.disabled = true;
     console.log(answer.disabled)
@@ -144,13 +140,3 @@ function equal_enable() {
     var answer = document.getElementById("equal");
     answer.disabled = false;
 }
-
-// function delbtn_disable(){
-//     var clear = document.getElementById("delete");
-//     clear.disabled = true;
-// }
-
-// function delbtn_enable(){
-//     var clear = document.getElementById("delete");
-//     clear.enable= true;
-// }
